7e8064c5bb62b7e8666ca427f15ea11cd9addb0a
Jesus Delmoral <jdelmoral@students.mchenry.edu>
2020-09-22 18:56:50 -0500

    i

--- src/hw_employees.kt
+++ src/hw_employees.kt
@@ -1,6 +1,8 @@
+//class
 class Employees(var empNum: Int,
                    var empName: String?,
                    var empShift: Int){
+// displays the class
     fun printAll(){
         println("\nEmployee Number: " + empNum.toString() + "\nEmployee Name: " + empName +
                 "\nEmployee Shift: " + empShift)
--- src/hw_main.kt
+++ src/hw_main.kt
@@ -1,7 +1,9 @@
 fun main(){
+    // employees
     val person1 = Employees(1, "Sam", 3 )
     val person2 = Employees(2, "Nick", 2 )
     val person3 = Employees(3, "Pedro", 1 )
+    //prints employees
     person1.printAll()
     print("\n")
     person2.printAll()
